d5b693d8d1b5a8fe1b8b68553238713f7d145b7a
Will Dixon <dixonwille@hotmail.com>
2018-10-02 21:24:14 -0400

    Get reveal.js working with Vue.js (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="reveal">
+      <div class="slides">
+        <section>Single Horizontal Slide</section>
+        <section>
+          <section>Vertical Slide 1</section>
+          <section>Vertical Slide 2</section>
+        </section>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Reveal from "reveal.js/js/reveal";
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  mounted() {
+    Reveal.initialize();
   }
 };
 </script>
 
 <style lang="scss">
+@import url("../node_modules/reveal.js/css/reveal.css");
+@import url("../node_modules/reveal.js/css/theme/black.css");
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
