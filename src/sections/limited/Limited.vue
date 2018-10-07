e85bf507291fea586750322ec3b85739a59378b2
Will Dixon <dixonwille@hotmail.com>
2018-10-07 18:11:21 -0400

    Finish the limited slides (#13)

--- src/sections/limited/Limited.vue
+++ src/sections/limited/Limited.vue
@@ -5,7 +5,7 @@
       h2 Limited Programming
       .center
         h4 A computer only does what it is told. No More. No less.
-    slide(:steps=4, enter="fadeInDown", leave="fadeOutDown")
+    slide(:steps=7, enter="fadeInDown", leave="fadeOutDown")
       h2 What is Programming?
       .center
         eg-transition(enter="bounceInLeft", leave="bounceOutLeft")
@@ -16,7 +16,11 @@
             p Bossing your computer around.
             eg-transition(enter='fadeIn')
               p(v-if="step >= 4") Telling a robot how to do certain tasks.
-    slide(:steps=11, enter="fadeInUp", leave="fadeOutUp")
+            eg-transition(enter='fadeIn')
+              p(v-if="step >= 5") Research
+                span(v-if="step >= 6") , a lot of
+                b(v-if="step >= 7")  RESEARCH!
+    slide(:steps=12, enter="fadeInUp", leave="fadeOutUp")
       h2 What can be programed?
       .center
         p(v-if="step === 1") Anything
@@ -42,23 +46,26 @@
             p A Wallet*?
             img(src="https://i.imgur.com/6fJrpDD.jpg", height="250px")
         eg-transition(enter="bounceInRight", leave="bounceOutLeft")
-          .subslide(v-if="step >= 10")
-            p(v-if="step === 10") And of course
-            p(v-if="step >= 11") A Robot
-            img(src="https://i.imgur.com/Gc9Wq8Z.jpg", height="250px", v-if="step >= 11")
+          .subslide(v-if="step >= 10 && step <= 11")
+            p(v-if="step === 10") And
+            p(v-if="step === 11") Of Course
+        eg-transition(enter="bounceInRight", leave="bounceOutLeft")
+          .subslide(v-if="step >= 12")
+            p A Robot
+            img(src="https://i.imgur.com/Gc9Wq8Z.jpg", height="250px")
     slide(:steps=15, enter="fadeInDown", leave="fadeOutDown")
       h2 Platforms available to us
       .center
         eg-transition(enter="bounceInRight", leave="bounceOutLeft")
           .subslide(v-if="step >= 2 && step <= 5")
-            h4(v-if="step >= 2")  Robo Rio
+            h4(v-if="step >= 2").trimBottom  Robo Rio
             img(v-if="step === 2", src="https://i.imgur.com/LjuxlIX.jpg", height="250px")
             p(v-if="step >= 3") Main computer of the robot
             p(v-if="step >= 4") Must control anything that moves on the robot
             p(v-if="step >= 5") Runs linux
         eg-transition(enter="bounceInRight", leave="bounceOutLeft")
           .subslide(v-if="step >= 6 && step <= 10")
-            h4(v-if="step >= 6") Arduino
+            h4(v-if="step >= 6").trimBottom Arduino
             img(v-if="step === 6", src="https://i.imgur.com/oFLXS67.jpg", height="250px")
             p(v-if="step >= 7") Is a micro-controller (meaning no operating system)
             p(v-if="step >= 8") Really good at reading sensors
@@ -66,7 +73,7 @@
             p(v-if="step >= 10") Difficult to get the values to another device (learned this last year)
         eg-transition(enter="bounceInRight", leave="bounceOutLeft")
           .subslide(v-if="step >= 11 && step <= 15")
-            h4(v-if="step >= 11") Raspberry Pi
+            h4(v-if="step >= 11").trimBottom Raspberry Pi
             img(v-if="step === 11", src="https://i.imgur.com/MjsvcVU.jpg", height="250px")
             p(v-if="step >= 12") A small computer
             p(v-if="step >= 13") Runs linux
@@ -105,7 +112,6 @@
                       Defensive().start() <eg-code-comment :active="step >= 13" enter="fadeInRight"> Anything else (middle position, left but our side is not lit).</eg-code-comment>
                   }
               }
-
     slide(:steps=14, enter="fadeInUp", leave="fadeOutUp")
       h2 C++ (Arduino)
       .center
@@ -142,29 +148,50 @@
                 }
                 return distance; <eg-code-comment :active="step >= 14" enter="fadeInRight"> Return distance if we think it is a good reading.</eg-code-comment>
               }
-
-    slide
-      h3 null reference is not set to an instance of an object
-      p.center: small Things I am still working on
-      .quarter
-        ul
-          li Sensors
-            ul
-              li Thermometers
-              li Ultrasonics
-              li Lidar
-              li Camera
-              li Pressure
-              li Proximity
-              li Potentiometers
-              li Limit Switch
-          li Expand on second slide?
-      .quarter
-        ul
-          li Go through and put speaker notes
-          li See if I can get speaker view to have a timer on each slide for me
-          li Redesign platform slide and subslides
-          li Is there any fun use with forms for these slides?
+    slide(:steps=5, enter="fadeInDown", leave="fadeOutDown")
+      h2.trimBottom Sensors
+      eg-transition(enter="fadeInLeft")
+        .quarter(v-if="step >= 2")
+          img(src="https://i.imgur.com/zLQre6a.jpg", height="100px")
+          h4 Temperatur
+          p Thermistor
+          p Analog Themometer
+      eg-transition(enter="fadeInRight")
+        .quarter(v-if="step >= 3")
+          img(src="https://i.imgur.com/mSizqU0.jpg", height="100px")
+          h4 Distance
+          p Ultrasonic
+          p Lidar
+      eg-transition(enter="fadeInLeft")
+        .quarter(v-if="step >= 4")
+          img(src="https://i.imgur.com/TlJkFGP.png", height="100px")
+          h4 Position
+          p Encoders
+          p Potentiometers
+      eg-transition(enter="fadeInRight")
+        .quarter(v-if="step >= 5")
+          img(src="https://i.imgur.com/ltwSeDo.jpg", height="100px")
+          h4 Camera
+          p Color
+          p Infrared
+    slide(:steps=7, enter="fadeInUp", leave="fadeOutUp")
+      h3(v-if="step === 1") If you have not been listening...
+      h3(v-if="step >= 2") Here are a few Takeaways
+      eg-transition(enter="bounceInRight", leave="bounceOutRight")
+        p(v-if="step >= 3") We have to build one Robot
+          span(v-if="step >= 4") , but that doesn't mean what have to build it all at once
+      eg-transition(enter="bounceInRight", leave="bounceOutRight")
+        p(v-if="step >= 5") The sooner a programmer can get to the module, the sooner it can be tested
+      eg-transition(enter="bounceInRight", leave="bounceOutRight")
+        p(v-if="step >= 6") Sensors are important for autonomous mode
+      eg-transition(enter="bounceInRight", leave="bounceOutRight")
+        p(v-if="step >= 7") It may sound easy to you, but think about it in it's most basic components and it can get complex to program
+    slide(enter="fadeInDown", leave="fadeOutDown")
+      h1 Q/A
+      p Code:
+        a(href="https://github.com/technomancers") https://github.com/technomancers
+      p Email:
+        a(href="mailto:will@willd.io") will@willd.io
 </template>
 
 <script>
@@ -185,7 +212,11 @@ export default {
         "https://i.imgur.com/Gc9Wq8Z.jpg",
         "https://i.imgur.com/LjuxlIX.jpg",
         "https://i.imgur.com/oFLXS67.jpg",
-        "https://i.imgur.com/MjsvcVU.jpg"
+        "https://i.imgur.com/MjsvcVU.jpg",
+        "https://i.imgur.com/zLQre6a.jpg",
+        "https://i.imgur.com/mSizqU0.jpg",
+        "https://i.imgur.com/TlJkFGP.png",
+        "https://i.imgur.com/ltwSeDo.jpg"
       ]
     };
   },
@@ -204,3 +235,26 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+h1,
+h2,
+h3,
+h4 {
+  &.trimBottom {
+    margin-bottom: 0.4em;
+  }
+}
+.quarter {
+  text-align: center;
+  h4 {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  p {
+    margin-top: 0;
+    margin-bottom: 0;
+    text-align: center;
+  }
+}
+</style>
